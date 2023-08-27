fcc8d2ff6bae2c4229a5670f161983b021472097
yashsagar <sagar.sagar75@gmail.com>
2023-08-28 00:28:01 +0530

    basic layout update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
-import "./index.css";
+import Dies from "./Dies.js";
 
 function App() {
   return (
-    <div>
-      <h1>test why its not working</h1>
+    <div id="mainContainer">
+      <h1>Tenzies</h1>
+      <p className="textArea">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <Dies />
+      <button id="btn">Roll</button>
     </div>
   );
 }
---
+++ src/Dies.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Dies() {
+  return (
+    <div id="diesContainer">
+      <div className="die">1</div>
+      <div>2</div>
+      <div>3</div>
+    </div>
+  );
+}
